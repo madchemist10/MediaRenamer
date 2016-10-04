a1188bde6a637ac39f63f1f6256d9186b0e21d7b
MadChemist <2012kemp1234@gmail.com>
2016-10-04 07:22:12 -0500

    Added utility class with private constructor.

---
+++ src/utilities/Utilities.java
@@ -0,0 +1,14 @@
+package utilities;
+
+/**
+ * Utility class to contain helper methods needed throughout
+ * code. All methods defined here should be static and self contained.
+ */
+public class Utilities {
+
+    /**
+     * Private constructor to defeat instantiation.
+     */
+    private Utilities(){
+    }
+}
