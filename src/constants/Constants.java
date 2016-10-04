ebd58eb971092854f472bb27d26cc00c77d26929
MadChemist <2012kemp1234@gmail.com>
2016-10-04 07:27:34 -0500

    Added Settings file path constant.

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -6,4 +6,6 @@ package constants;
 public class Constants {
     /**Name of the log file for this application.*/
     public static final String LOG_FILE = "mediaRename.log";
+    /**Name of the settings file read in by the system.*/
+    public static final String SETTINGS_FILE = "settings.txt";
 }
