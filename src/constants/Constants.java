7f4c90be9aea7ea792f5d11bced03ffa09e35592
MadChemist <2012kemp1234@gmail.com>
2016-10-04 07:17:38 -0500

    Added constants file to maintain strict reference to all constant strings and values.

---
+++ src/constants/Constants.java
@@ -0,0 +1,9 @@
+package constants;
+
+/**
+ * Constants used throughout this application.
+ */
+public class Constants {
+    /**Name of the log file for this application.*/
+    public static final String LOG_FILE = "mediaRename.log";
+}
